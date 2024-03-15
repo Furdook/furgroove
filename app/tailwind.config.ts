898a8eab192df305e4cc7ab1a1e355a4ef927cac
Timon Alexander Selnes <timonas@stud.ntnu.no>
2024-03-15 07:40:50 +0100

    Init Next rewrite

---
+++ app/tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    colors: {
+      primary: {
+        900: "#1d1527",
+        800: "#33283d",
+      },
+      pink: "#e25691",
+      blue: "#77f2da",
+      yellow: "#eeae43",
+      white: "#e8e4dd",
+    },
+  },
+} satisfies Config;
+
+export default config;
