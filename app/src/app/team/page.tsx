4e791a83b02adff9d22d49a4ebe5ef28a24b84fd
Timon Alexander Selnes <timonas@stud.ntnu.no>
2024-03-16 18:24:52 +0100

    Improve padding method on team page

--- app/src/app/team/page.tsx
+++ app/src/app/team/page.tsx
@@ -3,10 +3,8 @@ import { team } from "@/constants";
 
 export default function Team() {
   return (
-    <main className="mx-auto my-12 flex max-w-4xl flex-col gap-12">
-      <h1 className="mb-6 w-full text-3xl text-yellow max-[935px]:pl-6">
-        Meet the Team!
-      </h1>
+    <main className="mx-auto my-12 flex max-w-[65.5rem] flex-col gap-12">
+      <h1 className="mb-6 w-full pl-6 text-3xl text-yellow">Meet the Team!</h1>
       <section className="w-full flex-col gap-12">
         {team.map((member, index) => {
           return <Artist key={index} {...member} />;
