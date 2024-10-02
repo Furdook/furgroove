6e111b8cb72299edfdb8e2693d425a71f713c884
Timon Alexander Selnes <timonas@stud.ntnu.no>
2024-10-02 08:02:32 +0200

    Update archive information and structure

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,8 +1,7 @@
 import Breadcrumb from "@/components/Breadcrumb";
-import Card from "./_components/card";
 import HeaderImage from "@/components/HeaderImage";
 
-export type ArchiveItem = {
+type ArchiveItem = {
   title: string;
   date: string;
   description: string;
@@ -16,7 +15,7 @@ const archiveItems: ArchiveItem[] = [
     title: "FurGroove: Set Sail",
     date: "10th of August 2024",
     description:
-      "This event was hosted crusing through the canals of Rotterdam on a boat from BOOT10, the trip lasted for four hours exploring the canals as fluffy as possible. ",
+      "This event was hosted crusing through the canals of Rotterdam on a boat from BOOT10, the trip lasted for four hours exploring the canals as fluffy as possible. With over 200 attendees, many in fursuits, this was the place to be!",
     artists: ["Beatbird", "YellowStreak", "Menos"],
     location: "Rotterdam",
     gallery: "/",
@@ -25,7 +24,7 @@ const archiveItems: ArchiveItem[] = [
     title: "FurGroove",
     date: "29th of January 2024",
     description:
-      "This first edition of FurGroove was hosted at the iconic Fort33 venue, lasting from 20:00 to 02:00",
+      "This first edition of FurGroove was hosted at the iconic Fort33 venue, lasting from 20:00 to 02:00. With over 150 attendees, this was a great beginning to the FurGroove series.",
 
     artists: ["Beatbird", "Djem", "Stripe", "Menos"],
     location: "Leusden",
@@ -41,18 +40,49 @@ export default function Home() {
         <Breadcrumb currentPage="Archive" />
       </header>
       <h2 className="ml-4 text-pink">Archive</h2>
-      <section className="rounded bg-primary-800/50 p-4 shadow-md">
-        <h3 className="text-xl font-light uppercase text-blue">
-          Prior events hosted by FurGroove
-        </h3>
-        <p>
-          Here you'll find a summary of all of our passed events, from our very
-          first in Leusden 2024 to our most recently held event.
-        </p>
-      </section>
       {archiveItems.map((item) => (
         <Card key={item.title} archivedEvent={item} />
       ))}
     </main>
+  );
+}
+
+import { Image } from "lucide-react";
+
+function Card({ archivedEvent }: { archivedEvent: ArchiveItem }) {
+  return (
+    <section className="rounded bg-primary-800/50 p-4 shadow-md">
+      <h3 className="text-xl font-light uppercase text-blue">
+        {archivedEvent.title}
+      </h3>
+      <h4 className="mb-4 uppercase ">{archivedEvent.date}</h4>
+      <p>{archivedEvent.description}</p>
+      <p className="mt-4 ">Artists who performed at this event:</p>
+      <div className="mb-4 flex gap-1">
+        {archivedEvent.artists.map((artist) => {
+          return (
+            <p key={artist}>
+              {artist}{" "}
+              {archivedEvent.artists.indexOf(artist) ===
+              archivedEvent.artists.length - 1
+                ? ""
+                : "-"}
+            </p>
+          );
+        })}
+      </div>
+      <div className="flex gap-4">
+        <p className="mt-auto flex gap-2 text-lg text-primary-300/50">
+          {archivedEvent.location}
+        </p>
+        <a
+          href={archivedEvent.gallery}
+          className="path ml-auto flex gap-2 rounded-inner bg-primary-800/50 px-4 py-1.5 transition-colors duration-200 hover:bg-primary-800 hover:shadow-xl"
+        >
+          <Image size={20} className="mt-0.5" />
+          Picture Gallery
+        </a>
+      </div>
+    </section>
   );
 }
